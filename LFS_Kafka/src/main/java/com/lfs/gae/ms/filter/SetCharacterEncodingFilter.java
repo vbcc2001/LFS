4de4ca52a659fe2a692eca6af53e536259f940ff
vbcc2001 <vbcc2001@gmail.com>
2016-11-01 17:15:13 +0800

    1
    1

---
+++ LFS_Kafka/src/main/java/com/lfs/gae/ms/filter/SetCharacterEncodingFilter.java
@@ -0,0 +1,32 @@
+package com.lfs.gae.ms.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * 描述:编码过滤器
+ */
+public class SetCharacterEncodingFilter implements Filter{
+	protected String encoding = "UTF-8";
+	@Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain){
+        try{
+        	request.setCharacterEncoding(encoding);
+        	response.setCharacterEncoding(encoding);
+        	response.setContentType("text/html; charset=" + encoding);
+            chain.doFilter(request, response);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+	@Override
+	public void destroy() {
+	}
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}	
+}
