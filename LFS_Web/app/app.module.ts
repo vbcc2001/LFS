422d89d2944ac9a48c6f14fe155dfd152848de50
vbcc2001 <vbcc2001@gmail.com>
2016-11-11 18:21:05 +0800

    1
    1

---
+++ LFS_Web/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }   from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule ]
+})
+export class AppModule { }
