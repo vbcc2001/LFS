0a584e84c3fad3a9761f03f847607303615f7cfd
vbcc2001 <vbcc2001@gmail.com>
2017-05-17 10:26:08 +0800

    1
    1

--- LFS_Web_Java/target/resinctl.bat
+++ LFS_Web_Java/target/resinctl.bat
@@ -1,8 +1,9 @@
 @echo off
 set PROJECT_PATH=B:\LFS\LFS_Web_Java
 set APP_HOME=%PROJECT_PATH%\target
+set APP_PORT=3000
 set JAVA_HOME=%PROJECT_PATH%\target\jdk1.8.0_121\jre
 set RESIN_HOME=%PROJECT_PATH%\target\resin-pro-3.1.12
 set APP_NAME=LFS_Web_Java
 title %APP_NAME%
-%JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -Dapp.name=%APP_NAME% -Dapp.home=%APP_HOME% -jar %RESIN_HOME%\lib\resin.jar -resin-home %RESIN_HOME% -conf %APP_HOME%\resin.xml -server %APP_NAME%
+%JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -Dapp.name=%APP_NAME% -Dapp.home=%APP_HOME% -Dapp.port=%APP_PORT% -jar %RESIN_HOME%\lib\resin.jar -resin-home %RESIN_HOME% -conf %APP_HOME%\resin.xml -server %APP_NAME%
